fb5288a45e81d4ef51655d5a2a1b258ad35a376f
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-23 13:49:29 +0300

    img compoenent added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,21 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
 export const Navbar = () => {
-    return (
-        <div>
-            <h1>Navbar</h1>
-        </div>
-    )
-}
+  return (
+    <div className="navbox">
+      <div className="leftside">
+        <img
+          src="https://d1whtlypfis84e.cloudfront.net/guides/wp-content/uploads/2018/11/23065530/ecommerce-1992280_960_720-300x225.png"
+          alt=""
+        />
+      </div>
+      <div className="rightside">
+        <Link to="/signup">SIGN UP</Link>
+        <Link to="/login" className="navlink">
+          LOGIN
+        </Link>
+      </div>
+      <h1>Navbar</h1>
+    </div>
+  );
+};
